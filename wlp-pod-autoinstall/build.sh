3ff3fbaf504ecc7fdfbb04e108935e3c57ec4598
Neil <maartenq@protonmail.com>
2025-01-15 20:30:32 +0100

    updated to always use latest git repo

--- wlp-pod-autoinstall/build.sh
+++ wlp-pod-autoinstall/build.sh
@@ -5,23 +5,28 @@ main_repo_bname="wlp-v2"
 core_plugins_repo="https://github.com/wlp-builders/whitelabelpress-wlp"
 core_plugins_dir=`realpath "wlp-core-plugin-cms-pack"`
 core_plugins_bname="wlp-core-plugin-cms-pack"
+zips_dir=`realpath "zips"`
 
-# Clone the main WLP repository if it doesn't already exist
-if [ ! -d "$main_repo_dir" ]; then
+if [ -d "$zips_dir" ]; then
+	rm "$zips_dir" -rf
+	mkdir "$zips_dir"
+fi
+if [ -d "$main_repo_dir" ]; then
+	rm "$main_repo_dir" -rf
+fi
+if [  -d "$core_plugins_dir" ]; then
+	rm "$core_plugins_dir" -rf
+fi
+
+# Always Clone the latest main WLP repository if it doesn't already exist
     echo "Cloning main repository..."
     git clone "$main_repo" "$main_repo_dir"
     zip -r zips/wlp-v2.zip "$main_repo_bname"
-else
-    echo "Main repository already exists, skipping clone."
-fi
 
 # Clone the core plugins repository if it doesn't already exist
-if [ ! -d "$core_plugins_dir" ]; then
-    echo "Cloning core plugins repository..."
-    git clone "$core_plugins_repo" "$core_plugins_dir"
-    zip -r zips/wlp-core-plugin-cms-pack.zip "$core_plugins_bname"
-else
-    echo "Core plugins repository already exists, skipping clone."
-fi
+echo "Cloning core plugins repository..."
+git clone "$core_plugins_repo" "$core_plugins_dir"
+zip -r zips/wlp-core-plugin-cms-pack.zip "$core_plugins_bname"
 
 podman build -t wlp-pod .
+
